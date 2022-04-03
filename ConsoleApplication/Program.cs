accf6179d78f399ad1a943342c2f6c344f34828f
Callum <callum.s.stacherski@gmail.com>
2022-04-03 13:42:40 +1000

    hi guys <3!

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Hello World!");
+            }
+        }
+    }
+}
