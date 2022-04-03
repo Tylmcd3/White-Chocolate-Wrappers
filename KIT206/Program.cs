d7f26507fc2032cbe1f59d2ae371ee7299c2f81c
jofbob <jofbob@hotmail.com>
2022-04-03 14:13:08 +1000

    add cool loop

--- KIT206/Program.cs
+++ KIT206/Program.cs
@@ -6,7 +6,11 @@ namespace KIT206
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for(int i=0; i < 69; i++)
+			{
+                Console.WriteLine("Hello World!");
+            }
+
         }
     }
 }
