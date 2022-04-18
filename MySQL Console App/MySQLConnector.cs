fd962f1761e9b8cff9572f95539ba079f303bf75
Tylmcd3 <tylerm@utas.edu.au>
2022-04-18 13:34:01 +1000

    Made Program.cs the original Program file from the first project. Created Storage Adapter and mysql to use as the new storage device (NOT WORKING YET)

---
+++ MySQL Console App/MySQLConnector.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data.SqlClient;
+
+namespace MySQL
+{
+    class MySQLConnector
+    {
+        private const string db = "gmis";
+        private const string user = "kit206g6";
+        private const string pass = "group6";
+        private const string server = "alacritas.cis.utas.edu.au";
+
+        static MySqlConnection DatabaseConnect()
+        {
+            string connectionString = String.Format("Database={0};Data Source={1};User Id={2};Password={3}", db, server, user, pass);
+            return new MySqlConnection(connectionString);
+        }
+
+
+    }
+}
