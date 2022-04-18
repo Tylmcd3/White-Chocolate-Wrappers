a378a9976c3080e4f29cacd1c1feb8ac6ef2470c
Tylmcd3 <tylerm@utas.edu.au>
2022-04-18 23:38:30 +1000

    Shuffled around the commented functions to organise them into the classes that they are called from and added a second Get function for groups to Get all groups in a list

--- MySQL Console App/StorageAdapter.cs
+++ MySQL Console App/StorageAdapter.cs
@@ -10,50 +10,37 @@ namespace KIT206
 {
     class StorageAdapter
     {
-        //public static List<Student> Students { get => Storage.Students; }
-        //public static List<StudentGroup> Groups { get => Storage.Groups; }
-        //public static List<Meeting> Meetings { get => Storage.Meetings; }
-        //public static List<Class> Classes { get => Storage.Classes; }
+        //Student Storage Functions
+
+        //I dont remember if we actually need this as we never actually create new users. 
+        //I wont delete it yet though
         //public static void AddStudent(Student student)
         //{
         //    Storage.AddStudent(student);
         //}
-        //public static void AddGroup(StudentGroup group)
-        //{
-        //    Storage.AddGroup(group);
-        //}
-        //public static void AddMeeting(Meeting meeting)
-        //{
-        //    Storage.AddMeeting(meeting);
-        //}
-        //public static void AddClasses(Class Class)
-        //{
-        //    Storage.AddClasses(Class);
-        //}
-        //public static void EditStudent(Student student)
-        //{
-        //    Storage.EditStudent(student);
-        //}
-        //public static void EditGroup(StudentGroup group)
-        //{
-        //    Storage.EditGroup(group);
-        //}
-        //public static void EditMeeting(Meeting meeting)
-        //{
-        //    Storage.EditMeeting(meeting);
-        //}
-        //public static void EditClasses(Class Class)
-        //{
-        //    Storage.EditClasses(Class);
-        //}
+        public static void EditStudentGroup(int id, int group_id)
+        {
+            MySqlConnection conn = MySQLConnector.DatabaseConnect();
+            string sqlcmd = ("UPDATE student SET group_id =" + group_id + "WHERE student_id = " + id);
 
+            MySqlCommand cmd = new MySqlCommand(sqlcmd, conn);
+            MySQLConnector.DBExecute(cmd, conn);
+        }
+        public static void EditStudentDetails(int id, string title, Campus campus, Category category, string email, string phone)
+        {
+            MySqlConnection conn = MySQLConnector.DatabaseConnect();
+            string sqlcmd = $"UPDATE student SET title ={title}, campus = {campus.ToString()}, phone = {phone}, email = {email},category = {category.ToString()} WHERE student_id = " + id;
+
+            MySqlCommand cmd = new MySqlCommand(sqlcmd, conn);
+            MySQLConnector.DBExecute(cmd, conn);
+        }
         public static Student GetStudent(int id)
         {
             MySqlConnection conn = MySQLConnector.DatabaseConnect();
             string sqlcmd = ("select * from student WHERE student_id = " + id);
             MySqlCommand cmd = new MySqlCommand(sqlcmd, conn);
             Student student = null;
-            MySqlDataReader rdr = MySQLConnector.DBQuery(cmd, conn);
+            MySqlDataReader rdr = MySQLConnector.DBReader(cmd, conn);
             while (rdr.Read())
             {
                 if (rdr[7] != "")
@@ -68,14 +55,23 @@ namespace KIT206
             MySQLConnector.DBClose(rdr, conn);
             return student;
         }
+        //Group Functions
+        //public static void AddGroup(StudentGroup group)
+        //{
+        //    Storage.AddGroup(group);
+        //}
+        //public static void EditGroup(StudentGroup group)
+        //{
+        //    Storage.EditGroup(group);
+        //}
         public static StudentGroup GetGroup(int id)
         {
             MySqlConnection conn = MySQLConnector.DatabaseConnect();
-            string sqlcmd = "SELECT * FROM `studentGroup` WHERE group_id="+id;
+            string sqlcmd = "SELECT * FROM `studentGroup` WHERE group_id=" + id;
             MySqlCommand cmd = new MySqlCommand(sqlcmd, conn);
             StudentGroup group = null;
 
-            MySqlDataReader rdr = MySQLConnector.DBQuery(cmd, conn);
+            MySqlDataReader rdr = MySQLConnector.DBReader(cmd, conn);
             while (rdr.Read())
             {
                 group = new StudentGroup((string)rdr[1], (int)rdr[0]);
@@ -85,12 +81,45 @@ namespace KIT206
             MySQLConnector.DBClose(rdr, conn);
             return group;
         }
+        public static List<StudentGroup> GetGroups()
+        {
+            MySqlConnection conn = MySQLConnector.DatabaseConnect();
+            string sqlcmd = "SELECT * FROM `studentGroup`";
+            MySqlCommand cmd = new MySqlCommand(sqlcmd, conn);
+            List<StudentGroup> groups = null;
 
+            MySqlDataReader rdr = MySQLConnector.DBReader(cmd, conn);
+            while (rdr.Read())
+            {
+                groups.Add(new StudentGroup((string)rdr[1], (int)rdr[0]));
+
+            }
+
+            MySQLConnector.DBClose(rdr, conn);
+            return groups;
+        }
+        //Meeting functions
+        //public static void AddMeeting(Meeting meeting)
+        //{
+        //    Storage.AddMeeting(meeting);
+        //}
+        //public static void EditMeeting(Meeting meeting)
+        //{
+        //    Storage.EditMeeting(meeting);
+        //}
         //public static Meeting GetMeeting(DateTime start)
         //{
         //    return Storage.GetMeeting(start);
         //}
-
+        //Class functions
+        //public static void AddClasses(Class Class)
+        //{
+        //    Storage.AddClasses(Class);
+        //}
+        //public static void EditClasses(Class Class)
+        //{
+        //    Storage.EditClasses(Class);
+        //}
         //public static Class GetClass(int id)
         //{
         //    return Storage.GetClass(id);
