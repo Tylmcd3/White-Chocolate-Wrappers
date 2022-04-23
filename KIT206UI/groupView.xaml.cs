a3457ecd80913dd58374db8a6be1fbca397f9458
Zanschlick <102491575+Zanschlick@users.noreply.github.com>
2022-04-23 14:01:02 +1000

    Add files via upload

--- KIT206UI/groupView.xaml.cs
+++ KIT206UI/groupView.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace plessel
+namespace KIT206UI
 {
     /// <summary>
     /// Interaction logic for groupView.xaml
