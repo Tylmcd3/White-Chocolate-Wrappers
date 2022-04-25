1f211f3ddedc79b932768d395b053b2f3008614e
Tylmcd3 <97385189+Tylmcd3@users.noreply.github.com>
2022-04-25 14:28:32 +1000

    Started our new  plan for our main UI by removing the main view and using the group view as the main view

--- KIT206UIApp/MainWindow.xaml.cs
+++ KIT206UIApp/MainWindow.xaml.cs
@@ -10,23 +10,24 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace KIT206.DatabaseApp.UI
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for groupView.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
-            Student_Controller controller = new Student_Controller();
-            foreach(Student student in controller.Students)
-            {
-                Meetings.Items.Add(student.ToString(""));
-            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
