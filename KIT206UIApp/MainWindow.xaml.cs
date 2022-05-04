9aa1dfa7bb6dd54c4da2ec04b4b00eb6fdb26f0a
Callum Stacherski <callums2@utas.edu.au>
2022-05-04 19:38:04 +1000

    setting default for when users dont have images

--- KIT206UIApp/MainWindow.xaml.cs
+++ KIT206UIApp/MainWindow.xaml.cs
@@ -50,6 +50,7 @@ namespace KIT206.DatabaseApp.UI
             if (student.CurrentStudent.Photo.Length < 100)
             {
                 var image = new Image();
+
                 string path = student.CurrentStudent.Photo.Remove(0, 1);
                 path = path.Remove(path.Length - 1, 1);
 
@@ -62,6 +63,14 @@ namespace KIT206.DatabaseApp.UI
             }
             else
             {
+                Image defaultImage = new Image();
+                defaultImage.Width = 50;
+                defaultImage.Margin = new Thickness(5);
+                BitmapImage bi = new BitmapImage();
+                bi.BeginInit();
+                bi.UriSource = new Uri(@"C:\Users\buste\source\repos\White-Chocolate-WrappersNEWNEWNEWNNEW\KIT206UIApp\twitter-avi-gender-balanced-figure.png", UriKind.RelativeOrAbsolute);
+                bi.EndInit();
+                defaultImage.Source = bi;
 
             }
 
